7ad2cc9fff87aebbc1e08ffd075d316c3dabb643
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-23 01:00:54 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import "./js/test_api"
+
+function App() {
+  return (
+    <>
+      <p>frontend works</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/js/test_api.js
@@ -0,0 +1,12 @@
+// fetch("https://api.demo-login-system.giuseppetavella.com/auth/register", {
+//     method: "POST",
+//     headers: {
+//         "content-type": "application/json"
+//     },
+//     body: JSON.stringify({
+//         "firstname": "Giuseppe",
+//         "lastname": "Tavella",
+//         "email": "giuseppetavella8@gmail.com",
+//         "password": "password"
+//     })
+// })
