30914fbfcdd5412f2b43b170303bb2c4eb082c94
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-23 08:16:09 +0200

    try fetch

--- src/js/test_api.js
+++ src/js/test_api.js
@@ -2,15 +2,16 @@ const apiUrl = import.meta.env.VITE_API_URL
 
 console.log(apiUrl)
 
-// fetch("https://api.demo-login-system.giuseppetavella.com/auth/register", {
-//     method: "POST",
-//     headers: {
-//         "content-type": "application/json"
-//     },
-//     body: JSON.stringify({
-//         "firstname": "Giuseppe",
-//         "lastname": "Tavella",
-//         "email": "giuseppetavella8@gmail.com",
-//         "password": "password"
-//     })
-// })
+fetch(apiUrl + "/auth/register", {
+  method: "POST",
+  headers: {
+    "content-type": "application/json",
+  },
+  body: JSON.stringify({
+    firstname: "Giuseppe",
+    lastname: "Tavella",
+    email: "giuseppetavella8@gmail.com",
+    password: "password",
+  }),
+})
+
