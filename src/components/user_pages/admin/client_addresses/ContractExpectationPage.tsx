63a21c119c0f684064c4ca4dff752ef482681aa6
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-05-22 12:44:32 +0200

    contract expectation: show textarea with expectations

--- src/components/user_pages/admin/client_addresses/ContractExpectationPage.tsx
+++ src/components/user_pages/admin/client_addresses/ContractExpectationPage.tsx
@@ -3,15 +3,16 @@ import {
     AppEvent,
     AppEventMessageType,
     AppRoutes, ClientAddressFromAPI,
-    ContractExpectationFromAPI,
+    ContractExpectationFromAPI, DAY_LABELS, DayOfWeek,
     UpdatedContractExpectationToAPI
 } from "../../../../js/my_types.ts";
 import {useNavigate, useParams} from "react-router-dom";
 import ClientAddressesAPI from "../../../../js/api/ClientAddressesAPI.ts";
 import BadRequestError from "../../../../js/exceptions/BadRequestError.ts";
 import AppEventDispatcher from "../../../../js/AppEventDispatcher.ts";
-import {Col, Container, Row} from "react-bootstrap";
+import {Alert, Button, Col, Container, Form, ListGroup, Row, Spinner} from "react-bootstrap";
 import AddShiftForm from "../add_shift/AddShiftForm.tsx";
+import LanguageHelper from "../../../../js/helpers/LanguageHelper.ts";
 
 const appEventDispatcher = AppEventDispatcher.getInstance()
 
@@ -60,8 +61,8 @@ export default function ContractExpectationPage() {
 
     const [clientAddress, setClientAddress] = useState(initialClientAddress)
     const [updatedContractExpectation, setUpdatedContractExpectation] = useState(initialContractExpectation)
-    const [isLoading, setIsLoading] = useState(true)
-    const [isError, setIsError] = useState(false)
+    // const [isLoading, setIsLoading] = useState(true)
+    // const [isError, setIsError] = useState(false)
 
     const params = useParams<RouteURLParams>()
 
@@ -143,6 +144,60 @@ export default function ContractExpectationPage() {
                     {/* fields */}
                     <Row>
                         <Col>
+
+                            <Form
+                                onSubmit={(e) => {
+                                    e.preventDefault()
+                                }}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        // handleAddShift(shiftToAPI)({ setIsError, setIsLoading, setFormValues, setShiftToAPI, setChecklistsFromAPI });
+                                    }
+                                }}>
+
+                                {/* the contract expectation was success */}
+                                {clientAddress.contractExpectation.success && (
+
+                                    <Row className={"d-flex justify-content-center"}>
+
+                                        <Col className={"col-lg-6"}>
+                                            <div className="mb-3">
+                                                <label htmlFor="comments" className="form-label">Aspettative di Contratto</label>
+                                                <textarea
+                                                    className="form-control"
+                                                    id="comments"
+                                                    rows={15}
+                                                    placeholder="Inserisci i tuoi comandi per l'AI"
+                                                    value={clientAddress.contractExpectation.detail?.expectations}
+                                                />
+                                            </div>
+                                        </Col>
+
+                                    </Row>
+
+                                )}
+
+                                {/* contract expectation was everything but success */}
+                                {!clientAddress.contractExpectation.success && (
+                                    <p>L'estrazione delle aspettative dal contratto o non esiste, è in lavorazione o non è andata a buon fine.</p>
+                                )}
+
+
+                                {/* submit */}
+                                <Row className={"mt-5"}>
+                                    <Col className="text-center">
+                                        <Button
+                                            // disabled={isLoading}
+                                            variant="primary"
+                                            onClick={() => {
+                                                // handleAddShift(shiftToAPI)({ setIsError, setIsLoading, setFormValues, setShiftToAPI, setChecklistsFromAPI });
+                                            }}
+                                        >
+                                            Modifica
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Form>
 
                         </Col>
                     </Row>
